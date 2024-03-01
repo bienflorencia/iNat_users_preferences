0bd3f52807b7ace562f4b4b0e505d0684da24138
Rodrigo Montiel <montielrodrigo0@gmail.com>
2024-03-01 10:41:12 -0300

    Update 2_observations_categorization.R

--- code/2_observations_categorization.R
+++ code/2_observations_categorization.R
@@ -1,7 +1,34 @@
 # PACKAGES & DATA --------------------------------------------------------------
 library(tidyverse)
 
+
 species_list <- read.csv("data/species_list.csv")
+
+
+# NATUY OBSERVATIONS (WITH RESEARCH GRADE) -------------------------------------
+
+## Users quantity
+species_list %>%
+  group_by(user_id) %>% 
+  count() %>% nrow()
+
+## Number of observed species
+species_list %>%
+  group_by(taxon_species_name) %>% 
+  count() %>% arrange(desc(n)) %>% nrow()
+
+
+## observations by Kingdom
+kingdom_obs <- species_list %>%
+  group_by(taxon_kingdom_name) %>% 
+  summarise("Number of observations"= n(),
+            "Number of species" = length(unique(taxon_species_name)))
+
+## observations by Class
+class_obs <- species_list %>%
+  group_by(taxon_class_name) %>% 
+  summarise("Number of observations"= n(),
+            "Number of species" = length(unique(taxon_species_name)))
 
 
 # GROUPS SELECTION -------------------------------------------------------------
@@ -13,11 +40,11 @@ animals <- species_list %>%
   filter(taxon_class_name == "Aves" | taxon_class_name == "Amphibia" | 
            taxon_class_name == "Mammalia" | taxon_class_name == "Reptilia")
 
-write.csv(animals,"data/tetrapods_list.csv")
+write.csv(animals,"data/animals_list.csv")
 
 
 plants <- species_list %>% 
   filter(taxon_family_name == "Fabaceae" | taxon_family_name == "Cactaceae" | 
            taxon_family_name == "Asteraceae"|taxon_family_name == "Solanaceae")
 
-write.csv(plants,"data.csv")
+write.csv(plants,"data/plants_list.csv")
