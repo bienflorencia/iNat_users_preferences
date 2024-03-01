8f08eea6bb220b4ee16719866b435f9f5a01329d
Rodrigo Montiel <montielrodrigo0@gmail.com>
2024-03-01 10:22:02 -0300

    Update 1_data_processing.R

--- code/1_data_processing.R
+++ code/1_data_processing.R
@@ -1,21 +1,23 @@
 # PACKAGES & DATA --------------------------------------------------------------
 library(tidyverse)
+library(sf)
+sf::sf_use_s2(FALSE)
 
 
 observations <- read_csv('data/observations_21-02-24.csv')
 
-# FILTERING OBSERVACIONS
 
-species_list <- observations %>% 
+# FILTERING OBSERVACIONS -------------------------------------------------------
+
+species_list <- observations %>% st_drop_geometry() %>% 
   select(observed_on, place_admin1_name, taxon_kingdom_name, taxon_phylum_name, 
          taxon_class_name,taxon_order_name, taxon_family_name, 
          taxon_genus_name,taxon_species_name, taxon_subspecies_name) %>% 
-  filter(quality_grade == "research" & !is.na(taxon_species_name) & 
-           taxon_species_name!="") %>%  
-  filter(str_count(scientific_name, "\\S+") == 2) #This allows us to select 
-                                                  #those records that have two 
-                                                  #words in the 
-                                                  #scientific_name field
+  filter(!is.na(taxon_species_name) & taxon_species_name!="") %>%  
+  filter(str_count(taxon_species_name, "\\S+") == 2) # This allows us to select 
+                                                     # those records that have  
+                                                     # two words in the 
+                                                     # species_name field
 
 write.csv(species_list,"data/species_list.csv")
 
