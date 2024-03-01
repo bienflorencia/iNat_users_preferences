a28780fd275f40e35c8aa7f171722465040c1f38
Rodrigo Montiel <montielrodrigo0@gmail.com>
2024-03-01 11:11:11 -0300

    Update 1_data_processing.R

--- code/1_data_processing.R
+++ code/1_data_processing.R
@@ -10,9 +10,10 @@ observations <- read_csv('data/observations_21-02-24.csv')
 # FILTERING OBSERVACIONS -------------------------------------------------------
 
 species_list <- observations %>% st_drop_geometry() %>% 
-  select(observed_on, place_admin1_name, taxon_kingdom_name, taxon_phylum_name, 
-         taxon_class_name,taxon_order_name, taxon_family_name, 
-         taxon_genus_name,taxon_species_name, taxon_subspecies_name) %>% 
+  select(user_id, observed_on, place_admin1_name, taxon_kingdom_name, 
+         taxon_phylum_name, taxon_class_name,taxon_order_name, 
+         taxon_family_name, taxon_genus_name,taxon_species_name, 
+         taxon_subspecies_name) %>% 
   filter(!is.na(taxon_species_name) & taxon_species_name!="") %>%  
   filter(str_count(taxon_species_name, "\\S+") == 2) # This allows us to select 
                                                      # those records that have  
