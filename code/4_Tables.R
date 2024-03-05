ce88c7e531a78fc617b0a6e2626e7a7826b5eaa9
Rodrigo Montiel <montielrodrigo0@gmail.com>
2024-03-05 15:00:39 -0300

    Create 4_Tables.R

---
+++ code/4_Tables.R
@@ -0,0 +1,34 @@
+# PACKAGES & DATA --------------------------------------------------------------
+library(tidyverse)
+
+plants <- read.csv("data/plants_list.csv")
+animals <- read.csv("data/animals_list.csv")
+
+# JOINING TABLES----------------------------------------------------------------
+
+## These tables were used in my thesis and have the information on the traits 
+## of each species
+plants_thesis <- 
+  read_csv("~/GitHub/TesisNaturalistaUY/datos/Tablas/Lista_Plantas_Final.csv")
+
+animals_thesis <- 
+  read_csv("~/GitHub/TesisNaturalistaUY/datos/Tablas/Lista_Tetrapodos_Final.csv")
+
+## Let's join them with the new tables for this article
+
+plants_thesis <- plants_thesis %>% 
+  select(taxon_species_name, Distribution, Habito1, IUCNglobal)
+animals_thesis <- animals_thesis %>% 
+  select(taxon_species_name, Distribution, Size, IUCNglobal)
+
+plants_traits <- left_join(plants, plants_thesis)
+animals_traits <- left_join(animals, animals_thesis)
+
+
+write.csv(plants_traits,"data/plants_traits.csv")
+write.csv(animals_traits,"data/animals_traits.csv")
+
+
+
+
+
