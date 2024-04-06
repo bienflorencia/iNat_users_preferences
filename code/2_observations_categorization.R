5be14594d408bb8cd21569c837c3c4de764c4c69
Rodrigo Montiel <montielrodrigo0@gmail.com>
2024-04-06 20:31:10 -0300

    Update 2_observations_categorization.R

--- code/2_observations_categorization.R
+++ code/2_observations_categorization.R
@@ -36,11 +36,22 @@ class_obs <- species_list %>%
 ## We are going to create two lists with all the species observed within 
 ## the most recorded groups: tetrapods and dicotyledons
 
+## This is to know the total number of observations we have from the two groups
+most_registered <- species_list %>% 
+  filter(taxon_class_name == "Aves" | taxon_class_name == "Amphibia" | 
+           taxon_class_name == "Mammalia" | 
+           taxon_class_name == "Reptilia" | 
+           taxon_family_name == "Fabaceae" | 
+           taxon_family_name == "Cactaceae" | 
+           taxon_family_name == "Asteraceae"|
+           taxon_family_name == "Solanaceae") %>% nrow()
+
+
 animals <- species_list %>% 
   filter(taxon_class_name == "Aves" | taxon_class_name == "Amphibia" | 
            taxon_class_name == "Mammalia" | 
            taxon_class_name == "Reptilia") %>% 
-  group_by(taxon_species_name) %>% count()
+  group_by(taxon_class_name, taxon_species_name) %>% count()
 
 write.csv(animals,"data/animals_list.csv")
 
@@ -49,6 +60,6 @@ plants <- species_list %>%
   filter(taxon_family_name == "Fabaceae" | taxon_family_name == "Cactaceae" | 
            taxon_family_name == "Asteraceae"|
            taxon_family_name == "Solanaceae") %>% 
-  group_by(taxon_species_name) %>% count()
+  group_by(taxon_family_name, taxon_species_name) %>% count()
 
 write.csv(plants,"data/plants_list.csv")
